2806e0b91e372dda09aae88d8a7939dfb25f16b3
P10R10 <asantiago@keemail.me>
2023-06-18 14:31:44 +0100

    programming language

--- your_own_programming_language.py
+++ your_own_programming_language.py
@@ -1,24 +1,58 @@
-def unpack(program: list) -> list[list]:
-    """
-    INPUT: list of strings containing the program
-    OUTPUT: list of lists with instructions and respective operands
-    """
-    return [line.split(" ") for line in program]
+from string import ascii_uppercase
+
+
+def execute(command: list, output: list, variables: dict):
+    if command[0] == "PRINT":
+        output.append(variables.get(command[1]))
+    if command[0] == "MOV":
+        variables[command[1]] = int(command[2])
+    if command[0] == "ADD":
+        variables[command[1]] += variables.get(command[2])
+    if command[0] == "SUB":
+        variables[command[1]] -= variables.get(command[2])
+    if command[0] == "MUL":
+        variables[command[1]] *= variables.get(command[2])
 
 
 def run(program: list):
-    for command in unpack(program):
-        print(command)
+    output = []
+    variables = {ch: 0 for ch in ascii_uppercase}  # init variables
+    commands = [line.split(" ") for line in program]
+    i = 0
+    while True:
+        if commands[i][0] == "END":  # exit loop with END
+            break
+        if len(commands[i]) == 1:  # this is a tag
+            variables[commands[i][0]] = i  # keeps current line in variables
+        else:
+            execute(commands[i], output, variables)
+        i += 1
+    return output
 
 
 if __name__ == "__main__":
-    program1 = []
-    program1.append("MOV A 1")
-    program1.append("MOV B 2")
-    program1.append("PRINT A")
-    program1.append("PRINT B")
-    program1.append("ADD A B")
-    program1.append("PRINT A")
-    program1.append("END")
-    result = run(program1)
+    # program1 = []
+    # program1.append("MOV A 1")
+    # program1.append("MOV B 2")
+    # program1.append("PRINT A")
+    # program1.append("PRINT B")
+    # program1.append("ADD A B")
+    # program1.append("PRINT A")
+    # program1.append("END")
+    # result = run(program1)
+    # print(result)
+
+    program2 = []
+    program2.append("MOV A 1")
+    program2.append("MOV B 10")
+    program2.append("begin:")
+    # program2.append("IF A >= B JUMP quit")
+    program2.append("PRINT A")
+    program2.append("PRINT B")
+    program2.append("ADD A 1")  # WHAT IS WRONG???
+    # program2.append("SUB B 1")
+    # program2.append("JUMP begin")
+    # program2.append("quit:")
+    program2.append("END")
+    result = run(program2)
     print(result)
